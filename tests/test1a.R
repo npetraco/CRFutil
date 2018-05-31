f8751e1915079df7728fffeb3a7209e3cfcd0726
npetraco <npetraco@gmail.com>
2018-05-31 18:04:33 -0400

    Remote update

--- tests/test1a.R
+++ tests/test1a.R
@@ -71,19 +71,31 @@ st.sp
 
 # Decorate potentials/energies with gRbase conventions:
 #pot.info <- make.gRbase.potentials(mc, node.names = gp@nodes)
-pot.info <- make.gRbase.potentials(mc, node.names = gp@nodes, state.nmes = c("+1","-1"))
+pot.info <- make.gRbase.potentials(mc, node.names = gp@nodes, state.nmes = c("1","2"))
 pot.info
 
 # Compute joint from potentials with gRbase
 gR.dist.info <- distribution.from.potentials(pot.info$node.potentials, pot.info$edge.potentials)
-as.data.frame(as.table(gR.dist.info$state.probs))
+gR.dist <- as.data.frame(as.table(gR.dist.info$state.probs))
+gR.dist
 
 # Compute joint from energies
 en.dist.info <- distribution.from.energies(st.sp, mc$edges, pot.info$node.energies, pot.info$edge.energies, energy, f0)
-cbind(st.sp,en.dist.info$state.probs)
+en.dist <- cbind(st.sp,en.dist.info$state.probs)
+en.dist
+
+# Check to make sure dists from both gR and en have the same state indices
+library(prodlim)
+
+re.arr.idxs <- sapply(1:nrow(st.sp), function(xx){row.match(x = st.sp[xx,] ,table =  gR.dist[,c(3,4,2,1)])})
+gR.dist[re.arr.idxs,]
+cbind(en.dist[,5], gR.dist[re.arr.idxs, 5], en.dist[,5] - gR.dist[re.arr.idxs, 5])
 
 
-class(gR.dist.info$state.probs)
-class(en.dist.info$state.probs)
+# Without prodlim
+# D,C,A,B is the order in gRdist
+gR.distQQ <- sapply(1:nrow(st.sp), function(xx){gR.dist.info$state.probs[st.sp[xx,4], st.sp[xx,3], st.sp[xx,1], st.sp[xx,2]]})
+#as.data.frame(as.table(gR.dist.info$state.probs))
 
-st.sp
+cbind(st.sp, en.dist.info$state.probs, gR.distQQ, en.dist.info$state.probs-gR.distQQ)
+
