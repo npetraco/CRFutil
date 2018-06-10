ad0aaba24d777765833da663403df0d53527c796
npetraco <npetraco@gmail.com>
2018-06-10 16:01:10 -0400

    Remote update

---
+++ R/UGM_MRF_PseudoNLL.R
@@ -0,0 +1,16 @@
+#' Experiment: Port of Schmidt UGM_CRF_PseudoNLL.m
+#'
+#' Experiment
+#'
+#' The function will XXXX
+#'
+#' @param XX The XX
+#' @return The function will XX
+#'
+#'
+#' @export
+UGM_MRF_PseudoNLL <- function(w,nInstances,crf,inferFunc) {
+  # UGM functionwas: UGM_loss(w,Xnode,Xedge,Y,nodeMap,edgeMap,edgeStruct)
+
+
+}
