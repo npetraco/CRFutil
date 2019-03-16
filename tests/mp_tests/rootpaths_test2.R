9e3ca0923d8ef003c60fd2d10efff7014a5a8b07
npetraco <npetraco@PPP.local>
2019-03-16 18:20:01 -0400

    Remote update

--- tests/mp_tests/rootpaths_test2.R
+++ tests/mp_tests/rootpaths_test2.R
@@ -4,5 +4,10 @@ library(CRFutil)
 grphf <- ~1:3 + 2:3 + 3:4 + 3:5 + 5:6 + 6:7
 
 # Find all paths from root to leaves:
-gplst <- get.root.paths(grphf,root.node = 6)
-paths.to.serial.scheds
+get.root.paths(grphf,root.node = 5)
+
+
+# Model with a loop:
+grphf <- ~1:2 + 2:3 + 3:4 + 4:1 + 1:5
+plot(ug(grphf))
+get.root.paths(grphf,root.node = 2)
