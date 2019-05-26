7472daf337ea04b8d7d325b3a6ce47c609984c98
npetraco <npetraco@gmail.com>
2019-05-26 14:07:07 -0400

    Remote update

--- tests/regression_tests/hojsgaard_model_tests/triangle_model/triangle_comparison_template2.R
+++ tests/regression_tests/hojsgaard_model_tests/triangle_model/triangle_comparison_template2.R
@@ -2,7 +2,7 @@
 # loglin gRim,         DONE
 # model matrix loglin, DONE
 # glm-poisson,         DONE, see triangle-model_loglin-modelmartix, bottom. Equivalent results to model matrix loglin
-# bayes logistic,
+# bayes logistic,      DONE
 # mle logistic,
 # CRF fit
 # PSL fit
@@ -10,6 +10,7 @@
 
 library(CRFutil)
 fpth <- "/home/npetraco/codes/R/CRFutil/tests/regression_tests/hojsgaard_model_tests/triangle_model/triangle_data/"
+#setwd(fpth)
 
 load(paste0(fpth,"triangle_loglin_dist.RData"))
 loglin.dist.info
@@ -23,23 +24,29 @@ empirical.dist.info
 load(paste0(fpth,"triangle_loglin_MM_dist.RData"))
 loglin.MM.dist.info
 
+load(paste0(fpth,"triangle_bayes-lr_dist.RData"))
+bayes.lr.dist.info
+bayes.lr.dist.info <- bayes.lr.dist.info[,c(2,3,1,4)] # Forgot to rearrange columns
+
 
 ref.states <- exact.dist.info[,1:3]
 loglin.rearr.idxs    <- sapply(1:nrow(ref.states), function(xx){row.match(ref.states[xx,], table = loglin.dist.info[,1:3])})
 emp.rearr.idxs       <- sapply(1:nrow(ref.states), function(xx){row.match(ref.states[xx,], table = empirical.dist.info[,1:3])})
 loglin.MM.rearr.idxs <- sapply(1:nrow(ref.states), function(xx){row.match(ref.states[xx,], table = loglin.MM.dist.info[,1:3])})
+bayes.lr.rearr.idxs  <- sapply(1:nrow(ref.states), function(xx){row.match(ref.states[xx,], table = bayes.lr.dist.info[,1:3])})
 
 
 loglin.rearr.idxs
 loglin.MM.rearr.idxs
 emp.rearr.idxs
+bayes.lr.rearr.idxs
 
 spcr <- rep(NA, nrow(ref.states))
 cbind(exact.dist.info[,1:3], spcr,
       loglin.dist.info[loglin.rearr.idxs,1:3], spcr,
       loglin.MM.dist.info[loglin.MM.rearr.idxs,1:3], spcr,
+      bayes.lr.dist.info[bayes.lr.rearr.idxs,1:3], spcr,
       empirical.dist.info[emp.rearr.idxs,1:3])
-
 
 loglin.dist.info    <- loglin.dist.info[loglin.rearr.idxs,]
 loglin.MM.dist.info <- loglin.MM.dist.info[loglin.MM.rearr.idxs,]
@@ -55,4 +62,5 @@ cbind(ref.states,
       exact.dist.info[,4],
       loglin.dist.info[,4],
       loglin.MM.dist.info[,4],
+      bayes.lr.dist.info[,4],
       empirical.dist.info[,4])
