ce8c434a2cda6ae5217804b9632b1ac339515e81
npetraco <npetraco@gmail.com>
2019-06-21 18:09:46 -0400

    Remote update

--- R/marginal.edge.plot_util.R
+++ R/marginal.edge.plot_util.R
@@ -30,10 +30,10 @@ marginal.edge.bayes.bels.plot <- function(posterior.edge.belief.info, type="X1|X
 
   cond.sect.nmes <- colnames(posterior.edge.belief.info[[distc.nme]])
   marg.sect.nmes <- colnames(posterior.edge.belief.info[[distm.nme]])
-  print(distc.nme)
-  print(cond.sect.nmes)
-  print(distm.nme)
-  print(marg.sect.nmes)
+  #print(distc.nme)
+  #print(cond.sect.nmes)
+  #print(distm.nme)
+  #print(marg.sect.nmes)
 
   # Shut off plot window if open
   if(!is.null(dev.list())){
@@ -90,5 +90,60 @@ marginal.edge.bayes.bels.plot <- function(posterior.edge.belief.info, type="X1|X
 
   print(paste0("If X1_||_X2 then: ",distc.nmes[1], " = ", distm.nmes[1]))
   print(paste0("                  ",distc.nmes[2], " = ", distm.nmes[2]))
+
+}
+
+#' marginal edge empirical probs
+#'
+#' Empirical Pr(X1), Pr(X2), Pr(X1,X2), Pr(X1|X2), Pr(X2|X1) estimates from edge data
+#' The function will XXXX
+#'
+#' @param XX The XX
+#' @return The function will XX
+#'
+#'
+#' @export
+marginal.edge.bayes.plot <- function(posterior.edge.pot.mat, type="pot", prob.level=0.95){
+
+  if(type == "pot") { # for poentials
+    tau1.loc    <- posterior.edge.pot.mat[,1]
+    tau2.loc    <- posterior.edge.pot.mat[,2]
+    omeg.loc    <- posterior.edge.pot.mat[,3]
+    xlabs       <- c(expression(e^{tau[1]}), expression(e^{tau[2]}), expression(e^{omega}))
+    mains       <- c("exp(tau1)", "exp(tau2)", "exp(omega)")
+    indep.indic <- 1
+  } else if(type == "logpot") { # for energies
+    tau1.loc    <- log(posterior.edge.pot.mat[,1])
+    tau2.loc    <- log(posterior.edge.pot.mat[,2])
+    omeg.loc    <- log(posterior.edge.pot.mat[,3])
+    xlabs       <- c(expression(tau[1]), expression(tau[2]), expression(omega))
+    mains       <- c("tau1", "tau2", "omega")
+    indep.indic <- 0
+  } else {
+    stop("Choose: pot or logpot for type!")
+  }
+
+  tau1.int   <- HPDI(samples = tau1.loc, prob = prob.level)
+  tau2.int   <- HPDI(samples = tau2.loc, prob = prob.level)
+  omeg.int   <- HPDI(samples = omeg.loc, prob = prob.level)
+
+
+  # Shut off plot window if open
+  if(!is.null(dev.list())){
+    dev.off()
+  }
+  par(mfrow=c(2,2)) # split into 4
+
+  hist(tau1.loc, xlab=xlabs[1], main=mains[1])
+  points(tau1.int, c(0,0), pch=16, col="blue")
+  abline(v=indep.indic, lwd=4, col="green")
+
+  hist(tau2.loc, xlab=xlabs[2], main=mains[2])
+  abline(v=indep.indic, lwd=4, col="green")
+  points(tau2.int, c(0,0), pch=16, col="blue")
+
+  hist(omeg.loc, xlab=xlabs[3], main=mains[3])
+  abline(v=indep.indic, lwd=4, col="green")
+  points(omeg.int, c(0,0), pch=16, col="blue")
 
 }
--- tests/marginal_edge_plot_test2a_bayescheck3.R
+++ tests/marginal_edge_plot_test2a_bayescheck3.R
@@ -2,17 +2,17 @@ library(CRFutil)
 library(rstanarm)
 library(rethinking)
 
+grf.eq <- ~X + Y + X:Y
+XY <- make.empty.field(graph.eq = grf.eq, parameterization.typ = "standard", plotQ = F)
 
-# Using the same node potentials (0.8,0.2), (0.5,0.5), what does the re-scaled edge potential look like under independence??
-num.samps <- 100
+XY$node.pot[1,] <- c(0.8,0.2)
+XY$node.pot[2,] <- c(1-0.37,0.37)
+XY$edge.pot[[1]][1,1] <- XY$edge.pot[[1]][2,2] <- 1 # ******* theta true 11,22 is theta-times more likely than 12 or 21
+XY$node.pot
+XY$edge.pot
 
-# Samples are independent
-samps <- cbind(
-  1+rbinom(n = num.samps, size = 1, prob = 0.2),
-  1+rbinom(n = num.samps, size = 1, prob = 0.37)
-)
-
-dev.off()
+num.samps <- 500
+samps     <- sample.exact(XY, num.samps)
 mrf.sample.plot(samps)
 XY.emp.prs <- marginal.edge.emp.pr(samps) # Check sample margins. Marginals should be node pots and show independence
 XY.emp.prs
@@ -28,11 +28,13 @@ hist(log(eg.post.pots[,3]))
 
 # XXXXX
 junk <- marginal.edge.bels.bayes(eg.post.pots)
-head(junk$`Bel(X1,X2)`)
-head(junk$`Bel(X1)`)
-head(junk$`Bel(X2)`)
+#head(junk$`Bel(X1,X2)`)
 head(junk$`Bel(X1|X2)`)
+head(junk$`Bel(X1)`)
 head(junk$`Bel(X2|X1)`)
+head(junk$`Bel(X2)`)
 
-marginal.edge.bayes.bels.plot(junk, type="X1|X2", ymax=2500, edge.empirical.prob.info=NULL)
+marginal.edge.bayes.bels.plot(junk, type="X2|X1", ymax=3500, edge.empirical.prob.info=NULL)
 
+head(eg.post.pots)
+marginal.edge.bayes.plot(eg.post.pots, type = "logpot")
