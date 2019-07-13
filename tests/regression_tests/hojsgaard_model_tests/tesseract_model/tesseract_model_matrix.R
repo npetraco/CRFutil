4aba433d7274262dbf183050b35e4d20fd0011be
npetraco <npetraco@gmail.com>
2019-07-13 10:52:36 -0400

    Remote update

--- tests/regression_tests/hojsgaard_model_tests/tesseract_model/tesseract_model_matrix.R
+++ tests/regression_tests/hojsgaard_model_tests/tesseract_model/tesseract_model_matrix.R
@@ -29,8 +29,8 @@ node2nme
 
 # Make up random potentials/sample and return a CRF-object
 f0 <- function(y){ as.numeric(c((y==1),(y==2)))}
-tess <- make.empty.field(adj.mat = adj, parameterization.typ = "standard")
-set.seed(87)
+tess <- make.empty.field(adj.mat = adj, parameterization.typ = "standard", plotQ = T)
+#set.seed(87)
 tess$par <- runif(tess$n.par,-1.5,1.5)
 tess$par # "true" theta
 out.pot <- make.pots(parms = tess$par,  crf = tess,  rescaleQ = F, replaceQ = T)
@@ -64,39 +64,44 @@ sum(freqs)
 
 # Can't handle sampling on whole model
 # Just try non-zero freques
-nz.idxs <- which(freqs>0)
-X.red <- X.all[nz.idxs,]
-freqs.red <- freqs[nz.idxs]
-dim(X.red)
-length(freqs.red)
-hist(freqs.red)
-freqs.red
-max(freqs.red)
-X.red[which(freqs.red == max(freqs.red)), ]
+# nz.idxs <- which(freqs>0)
+# X.red <- X.all[nz.idxs,]
+# freqs.red <- freqs[nz.idxs]
+# dim(X.red)
+# length(freqs.red)
+# hist(freqs.red)
+# freqs.red
+# max(freqs.red)
+# X.red[which(freqs.red == max(freqs.red)), ]
 
 # Model Matrix with respect to graph ????
 M <- compute.model.matrix(configs=X.all, edges.mat=tess$edges, node.par = tess$node.par, edge.par = tess$edge.par, ff = f0)
 #M
 dim(M)
 
-M.red <- compute.model.matrix(configs=X.red, edges.mat=tess$edges, node.par = tess$node.par, edge.par = tess$edge.par, ff = f0)
-dim(M.red)
-M.red
+# M.red <- compute.model.matrix(configs=X.red, edges.mat=tess$edges, node.par = tess$node.par, edge.par = tess$edge.par, ff = f0)
+# dim(M.red)
+# M.red
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
 
 # Fit log(lam) = alp + M theta
-fpth <- "C:/Users/aliso/codes/CRFutil/tests/regression_tests/hojsgaard_model_tests/triangle_model/"
+#fpth <- "C:/Users/aliso/codes/CRFutil/tests/regression_tests/hojsgaard_model_tests/triangle_model/"
+fpth <- "/home/npetraco/codes/R/CRFutil/tests/regression_tests/hojsgaard_model_tests/triangle_model/"
 model.c <- stanc(file = paste0(fpth,"vanalla.poisson.regression2a.stan"), model_name = 'model')
 sm <- stan_model(stanc_ret = model.c, verbose = T)
 
-dat <- list(
-  p = ncol(M.red),
-  N = nrow(M.red),
-  y = freqs.red,
-  Mmodl = M.red
-)
-#dat
 
-bfit <- sampling(sm, data = dat, iter=10000, thin = 4, chains = 4)
+dat <- list(
+  p = ncol(M),
+  N = nrow(M),
+  y = freqs,
+  Mmodl = M
+)
+dat
+
+bfit <- sampling(sm, data = dat, iter=500, thin = 1, chains = 1)
 bfit
 
 theta <- extract(bfit, "theta")[[1]]
