d1fa772bb11d8de3842c57f7054a53ad83f6f425
npetraco <npetraco@gmail.com>
2021-05-29 15:47:26 -0400

    Remote update

--- tests/message-passing_tests/passing_loopy_test1.R
+++ tests/message-passing_tests/passing_loopy_test1.R
@@ -4,8 +4,8 @@ library(Rgraphviz)
 
 # Model:
 #grf <- ~1:2
-grf <- ~A:B
-#grf <- ~1:2 + 2:3 + 3:1
+#grf <- ~A:B
+grf <- ~1:2 + 2:3 + 3:1
 #grf <- ~A:B + B:C + C:A
 #grf <- ~A:B + B:C + C:A + B:D
 #grf <- ~A:B + B:C + C:A + B:FF + FF:G + G:B
@@ -28,3 +28,4 @@ msg.bxs       <- msg.cont.info$message.container
 msg.sch       <- msg.cont.info$message.schedule.mat
 
 msg.bxs
+msg.sch
